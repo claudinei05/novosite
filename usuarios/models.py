b763191b81b1eedc76ea3dd2ef6fbbd8091da746
claudinei05 <oliveiraclaudinei786@gmail.com>
2025-06-27 22:54:28 -0400

    27/06/2025-2

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Nota(models.Model):
     nome_aluno = models.CharField(max_length = 200)
     disciplina = models.CharField(max_length = 200)
-    nota_atividade = models.IntegerField(max_length = 0)
-    nota_trabalho = models.IntegerField(max_length =0)
-    nota_prova = models.IntegerField(max_length = 0)
+    nota_atividade = models.IntegerField(default= 0)
+    nota_trabalho = models.IntegerField(default=0)
+    nota_prova = models.IntegerField(default = 0)
     media = models.FloatField(blank = True, default = 0)
